7ef42963cf2b37ba90fe5213c68e5a5c058a7e71
SamuelMTL <95709293+SamuelMTL@users.noreply.github.com>
2023-08-23 10:04:19 -0300

    exercicio 1 feito

--- lista1/atividade1/verificaNumeroPrimo.java
+++ lista1/atividade1/verificaNumeroPrimo.java
@@ -5,13 +5,29 @@ package lista1.atividade1;
  */
 public class verificaNumeroPrimo {
 
-    public static void main(String args[]) {
-
-        System.out.println("Ola, mundo!");
-        verificarNumeroPrimo(12312312);
+    public static int verificadorNP(Integer n) {
+        if (n <= 1) {
+            return 0; // Números menores ou iguais a 1 não são primos
+        }
+        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return 0; // Números divisíveis por algum valor entre 2 e a raiz quadrada não são primos
+            }
+        }
+        
+        return 1; // Se o número não foi divisível por nenhum valor entre 2 e a raiz quadrada, é primo
     }
 
-    public static void verificarNumeroPrimo(Integer n){
-
+    public static void main(String[] args) {
+        Integer numero = 17; // Número que você deseja verificar
+        
+        int resultado = verificadorNP(numero);
+        
+        if (resultado == 1) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
     }
 }
